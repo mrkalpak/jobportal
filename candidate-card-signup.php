385e92bc435e7006a31e5abf8969790da72111f0
kalpak nemade <61384122+mrkalpak@users.noreply.github.com>
2024-02-08 18:06:46 +0530

    header.php

--- candidate-card-signup.php
+++ candidate-card-signup.php
@@ -138,114 +138,91 @@ if (isset($_POST['register'])) {
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php include './header.php'; ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Job Category</title>
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <!-- Bootstrap icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
-    <!-- Custom css -->
-    <link rel="stylesheet" href="assets/css/custom.css">
-    <link rel="stylesheet" href="./assets/css/login.css">
+<link rel="stylesheet" href="./assets/css/login.css">
+<!-- navbar  -->
+<?php include './navbar.php'; ?>
 
-    <!-- Jquery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</head>
+<div class="row  my-5 login-card shadow">
+    <div class="col-md-5 login-card-details">
+        <h4 class="mt-5">
+            Welcome to JOB FAIR INDIA
+        </h4>
+        <span style="font-size: smaller;">
+            Already have an account? <a href="./candidate-login.php">Log in</a>
+        </span>
 
-<body>
-    <!-- navbar  -->
-    <?php include './navbar.php'; ?>
+        <h5 class="fw-bold mt-5" style="color: var(--primary);">
+            Candidate Login
+        </h5>
+        <p style="color:red">
+            <?php
+            if (isset($error)) {
+                echo $error;
+            }
 
-    <div class="row  my-5 login-card shadow">
-        <div class="col-md-5 login-card-details">
-            <h4 class="mt-5">
-                Welcome to JOB FAIR INDIA
-            </h4>
-            <span style="font-size: smaller;">
-                Already have an account? <a href="./candidate-login.php">Log in</a>
-            </span>
+            ?>
+        </P>
 
-            <h5 class="fw-bold mt-5" style="color: var(--primary);">
-                Candidate Login
-            </h5>
-            <p style="color:red">
-                <?php
-                if (isset($error)) {
-                    echo $error;
-                }
+        <form class="mt-5" action="candidate-card-signup.php" method="POST">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name </label>
+                <input type="name" name="name" onkeydown="return /[a-zA-Z ]/i.test(event.key)" class="form-control" placeholder="Enter Name" id="name" value="<?php if (isset($error)) {
+                                                                                                                                                                    echo $name;
+                                                                                                                                                                } ?>" required>
 
-                ?>
-            </P>
-
-            <form class="mt-5" action="candidate-card-signup.php" method="POST">
-                <div class="mb-3">
-                    <label for="name" class="form-label">Name </label>
-                    <input type="name" name="name" onkeydown="return /[a-zA-Z ]/i.test(event.key)" class="form-control" placeholder="Enter Name" id="name" value="<?php if (isset($error)) {
-                                                                                                                                                                        echo $name;
-                                                                                                                                                                    } ?>" required>
-
-                </div>
-                <div class="mb-3">
-                    <label for="job-card-number" class="form-label">Job Card Number </label>
-                    <input type="text" name="jobcard" class="form-control" placeholder="Card Number" id="jobcard" value="<?php if (isset($error)) {
-                                                                                                                                echo $jobcard;
-                                                                                                                            } ?>" required>
-
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email </label>
-                    <input type="email" class="form-control" placeholder="Email Address" name="email" id="email" value="<?php if (isset($error)) {
-                                                                                                                            echo $email;
+            </div>
+            <div class="mb-3">
+                <label for="job-card-number" class="form-label">Job Card Number </label>
+                <input type="text" name="jobcard" class="form-control" placeholder="Card Number" id="jobcard" value="<?php if (isset($error)) {
+                                                                                                                            echo $jobcard;
                                                                                                                         } ?>" required>
 
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <div class="input-group">
-                        <input type="password" placeholder="********" name="password" class="form-control" id="password" required>
-                        <button class="btn btn-outline-eye toggle-password" type="button">
-                            <i class="bi bi-eye"></i>
-                        </button>
-                    </div>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email </label>
+                <input type="email" class="form-control" placeholder="Email Address" name="email" id="email" value="<?php if (isset($error)) {
+                                                                                                                        echo $email;
+                                                                                                                    } ?>" required>
 
-                </div>
-                <div class="mb-3">
-                    <label for="cnf_password" class="form-label">Confirm Password</label>
-                    <input type="password" placeholder="********" name="cpassword" class="form-control" id="cpassword" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <div class="input-group">
+                    <input type="password" placeholder="********" name="password" class="form-control" id="password" required>
+                    <button class="btn btn-outline-eye toggle-password" type="button">
+                        <i class="bi bi-eye"></i>
+                    </button>
                 </div>
 
-                <button type="submit" name="register" class="btn btn-theme-primary w-100 mb-5">Sign In</button>
-            </form>
+            </div>
+            <div class="mb-3">
+                <label for="cnf_password" class="form-label">Confirm Password</label>
+                <input type="password" placeholder="********" name="cpassword" class="form-control" id="cpassword" required>
+            </div>
 
-            <span class="" style="font-size: smaller;">
-                By login, you are agreeing to our Terms & Conditions and Privacy Policy.
-            </span>
-            <div class="mb-4"></div>
-        </div>
-        <div class="col-md-7 login-img-background py-5">
-            <img src="./assets/images/logo.png" class="ms-auto me-auto" width="300 rem" height="105px" alt="">
-            <img src="./assets/images/login-sideimage.png" width="300rem" alt="">
-        </div>
+            <button type="submit" name="register" class="btn btn-theme-primary w-100 mb-5">Sign In</button>
+        </form>
+
+        <span class="" style="font-size: smaller;">
+            By login, you are agreeing to our Terms & Conditions and Privacy Policy.
+        </span>
+        <div class="mb-4"></div>
     </div>
+    <div class="col-md-7 login-img-background py-5">
+        <img src="./assets/images/logo.png" class="ms-auto me-auto" width="300 rem" height="105px" alt="">
+        <img src="./assets/images/login-sideimage.png" width="300rem" alt="">
+    </div>
+</div>
 
 
-    <script src="./assets/js/showpassword.js"></script>
-    <!-- footer -->
-    <?php include './footer.php'; ?>
+<script src="./assets/js/showpassword.js"></script>
+<!-- footer -->
+<?php include './footer.php'; ?>
 
-    <!-- bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<!-- bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- company-joblist.php
+++ company-joblist.php
@@ -23,38 +23,11 @@ if ($result) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php include './header.php'; ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Job Category</title>
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <!-- Bootstrap icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
-    <!-- Custom css -->
-    <link rel="stylesheet" href="assets/css/custom.css">
     <!-- page -->
     <link rel="stylesheet" href="./assets/css/company-dashboard.css">
     <link rel="stylesheet" href="./assets/css/company-joblist.css">
-    <!-- Jquery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"> -->
-
-</head>
-
-<body>
     <!-- navbar -->
     <?php include './profileNavbar.php'; ?>
 
