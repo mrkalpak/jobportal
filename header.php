2b022fdd5baaafae9abdf3918fc2a350ae765d97
nihal5930 <nihal5930@gmail.com>
2024-03-04 17:58:55 +0530

    Host Testing

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Job Fairs</title>
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo1.png" type="image/x-icon">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
