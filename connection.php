140ef8ac38fde274d397a2fcfe8ac736c6496c2c
nihal5930 <nihal5930@gmail.com>
2024-03-05 11:27:02 +0530

    .env check

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $env = parse_ini_file('../.env');
 
 if(strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https') {
-    $con = mysqli_connect(`$env[HEADER_SERVER]`, $env['MySQL_SERVER_DB_USER_NAME'], $env['MySQL_SERVER_DB_PASSWORD'], $env['MySQL_SERVER_DB_NAME']);
+    $con = mysqli_connect(`$env[MySQL_SERVER_DB_HOST]`, $env['MySQL_SERVER_DB_USER_NAME'], $env['MySQL_SERVER_DB_PASSWORD'], $env['MySQL_SERVER_DB_NAME']);
 
     if (mysqli_connect_error()) {
         echo "<script>alert('cannot connect to the database');</script>";
