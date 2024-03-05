34ae9a8c1ec188fdd836696c3f8144cdef6ea5d8
nihal5930 <nihal5930@gmail.com>
2024-03-05 11:36:23 +0530

    check connection

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$env = parse_ini_file('../.env');
+$env = parse_ini_file('.env');
 
 if(strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https') {
     $con = mysqli_connect(`$env[MySQL_SERVER_DB_HOST]`, $env['MySQL_SERVER_DB_USER_NAME'], $env['MySQL_SERVER_DB_PASSWORD'], $env['MySQL_SERVER_DB_NAME']);
