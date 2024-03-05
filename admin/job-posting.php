8ea9ec39d6b13d6b3c4c7be2d7ed79799a757181
nihal5930 <nihal5930@gmail.com>
2024-03-05 14:58:25 +0530

    error check

--- admin/job-posting.php
+++ admin/job-posting.php
@@ -21,30 +21,28 @@ require('connection.php');
     $result = mysqli_query($con, $query);
     $result2 = mysqli_query($con, $query2);
     $result3 = mysqli_query($con, $query3);
-    // var_dump(mysqli_num_rows($result3)>0);
-    
-    if(mysqli_num_rows($result3)==0){
-        while($createUrl = $result3->fetch_assoc()) {
-            // var_dump($createUrl);
-            $jobTd=$createUrl['id'];
-            $jobTitle=$createUrl['jobTitle'];
-            $string = preg_replace('/\s+/', '', $jobTitle);
-            $url= $header."card-candidate-jobdetail.php?job=".$jobTd."";
-            //echo $url
-        }
-    }
+    // var_dump($result3);
+    if(mysqli_num_rows($result3)>0)
+    {    
+        while($createUrl = mysqli_fetch_assoc($result3)) {
+        // var_dump($createUrl);
+        $jobTd=$createUrl['id'];
+        $jobTitle=$createUrl['jobTitle'];
+        $string = preg_replace('/\s+/', '', $jobTitle);
+        $url= $header."card-candidate-jobdetail.php?job=".$jobTd."";
+        //echo $url
+    }}
     $phoneNumbersString = '';
 
     if (mysqli_num_rows($result)>0) {
         $row = mysqli_query($con,$query);
-
-
     } else {
         $NoData="No Data Found";
     }
 
     $row2 = mysqli_query($con,$query2);
-    if(mysqli_num_rows($result2)==0){
+    if(mysqli_num_rows($row2)>0)
+    {
         while($row2 = $result2->fetch_assoc()) {
             $phoneNumbersString .= $row2['phone'].',';
         }
@@ -123,7 +121,8 @@ require('connection.php');
                                     </tr>
                                     <?php
                                     // var_dump($createUrl['id'],$createUrl['jobTitle']);
-                                    if(mysqli_num_rows($result)==0){
+                                    if(mysqli_num_rows($result)>0)
+                                    {  
                                     while($row = $result->fetch_assoc()) {
                                         $query4 = "SELECT jobId FROM cardcandidate WHERE jobId=".$row['id']."";
                                         $result4 = mysqli_query($con, $query4);
